f24890486b3d22f9b47f8c397f2243925110bcd8
Holger Hees <holger.hees@gmail.com>
2018-12-03 19:17:34 +0100

    fixed heating temperature sensor handling

--- roles/vcontrold/templates/opt/heating_sensors/sensoren.bash
+++ roles/vcontrold/templates/opt/heating_sensors/sensoren.bash
@@ -2,17 +2,17 @@
 
 DIRNAME=`dirname "$0"`
 
-RESULT=`${DIRNAME}/TempCmd 2>&1`
-CODE=${PIPESTATUS[0]}
+RESULT="$(${DIRNAME}/TempCmd 2>&1)"
+#CODE=${PIPESTATUS[0]}
 
 #regex="\#([0-9]+).*: +([0-9]+\.[0-9]+)°C"                                                                                                
 regex="\#([0-9]+).*\+([0-9]+\.[0-9]+)°C"                                                                                                
 
 IFS=$'\n'
 
+COUNTER=0
 for line in $RESULT
 do
-	#echo $line
 	if [[ $line =~ $regex ]]
 	then                                                                                                                   
         sensor=${BASH_REMATCH[1]}                                                                                                                       
@@ -21,11 +21,20 @@ do
         
         if [ "$sensor" == "1" ]; then
 			curl -s -X PUT -H "Content-Type: text/plain" -d $value "http://127.0.0.1:8080/rest/items/Heating_Temperature_Pipe_Out/state"
+			let COUNTER=COUNTER+1
 		elif [ "$sensor" == "2" ]; then
 			curl -s -X PUT -H "Content-Type: text/plain" -d $value "http://127.0.0.1:8080/rest/items/Heating_Temperature_Pipe_In/state"
+			let COUNTER=COUNTER+1
 		fi
     fi
 done
+
+if [[  $COUNTER -lt 2 ]]
+then
+    echo "Could not read all sensors"
+    echo $RESULT
+    exit 1
+fi
 
 	#curl -s -X PUT -H "Content-Type: text/plain" -d $value "http://127.0.0.1:8080/rest/items/"$item"/state"
 #done
