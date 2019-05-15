937143cbea938d2eda33e18c7b58ccbede2c65b0
Holger Hees <holger.hees@gmail.com>
2019-05-15 17:24:02 +0200

    improved netdata config

---
+++ roles/netdata/templates/etc/netdata/send_notification.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+message=$3
+
+case $2 in
+"local6.info")
+  level="info"
+  ;;
+"local6.warning")
+  level="warning"
+  ;;
+"local6.crit")
+  level="crit"
+  ;;
+*)
+  level="crit"
+  message="UNKOWN LEVEL $2 : $message"
+  ;;
+esac
+
+#line=$(echo "$i" | xargs)
+echo "$message"  | systemd-cat -t netdata -p $level
