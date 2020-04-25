c413ebd8272f0be00ef6ad242c0b44492a52b297
Holger Hees <holger.hees@gmail.com>
2020-04-25 15:19:55 +0200

    initial cloud vpn based on wireguard

--- roles/wireguard_roadwarrior/templates/container/wg-startup.sh
+++ roles/wireguard_roadwarrior/templates/container/wg-startup.sh
@@ -21,9 +21,10 @@ then
     echo -n "Address = " >> wg0.conf
     echo -n $SERVER_ADDRESS >> wg0.conf
     echo "/24" >> wg0.conf
-    echo "ListenPort = 51820" >> wg0.conf
+    echo -n "ListenPort = " >> wg0.conf
+    echo $EXPOSED_PORT >> wg0.conf
     echo "SaveConfig = true" >> wg0.conf
-    
+
 {% for username in vault_usernames %}
     echo "[Peer]" >> wg0.conf
     echo -n "AllowedIPs = " >> wg0.conf
@@ -44,7 +45,7 @@ then
     echo -n "PrivateKey = " >> ./clients/wg_{{username}}.conf
     cat ./keys/client_{{username}}_privatekey >> ./clients/wg_{{username}}.conf
     echo -n "ListenPort = " >> ./clients/wg_{{username}}.conf
-    echo $PORT >> ./clients/wg_{{username}}.conf
+    echo $PUBLIC_PORT >> ./clients/wg_{{username}}.conf
     echo -n "DNS = " >> ./clients/wg_{{username}}.conf
     echo $DNS_SERVER >> ./clients/wg_{{username}}.conf
     
@@ -57,7 +58,7 @@ then
     echo -n "Endpoint = " >> ./clients/wg_{{username}}.conf
     echo -n $ENDPOINT >> ./clients/wg_{{username}}.conf
     echo -n ":" >> ./clients/wg_{{username}}.conf
-    echo $PORT >> ./clients/wg_{{username}}.conf
+    echo $PUBLIC_PORT >> ./clients/wg_{{username}}.conf
     #echo "PersistentKeepalive = 25" >> ./clients/wg_{{username}}.conf
 
     chmod 700 ./clients/wg_{{username}}.conf
