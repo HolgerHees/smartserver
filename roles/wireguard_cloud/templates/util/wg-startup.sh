026e778e687c237b99dec328ab2385ffacc2c856
Holger Hees <holger.hees@gmail.com>
2020-04-30 17:25:28 +0200

    more cloud improvements

--- roles/wireguard_cloud/templates/util/wg-startup.sh
+++ roles/wireguard_cloud/templates/util/wg-startup.sh
@@ -23,20 +23,6 @@ Endpoint = {{vault_cloud_vpn_networks[peer_network].peer.endpoint}}
 {% endif %}
 {% endfor %}"
 
-NEW_EXPORTS="{% for peer_network in vault_cloud_vpn_networks %}
-{% if peer_network != main_network %}
-{{raid_path}}{{data_dir_name}}/local/{{peer_network}} {{vault_cloud_vpn_networks[peer_network].peer.allowedIPs}}(rw,async)
-{% endif %}
-{% endfor %}"
-
-NEW_MOUNTS="{% for peer_network in vault_cloud_vpn_networks %}
-{% if peer_network != main_network %}
-{{vault_cloud_vpn_networks[peer_network].peer.nfsServer}}:{{raid_path}}{{data_dir_name}}/local/{{peer_network}} {{raid_path}}{{data_dir_name}}/remote/{{peer_network}} nfs rw 0 0
-{% endif %}
-{% endfor %}"
-
-#CONFIG="${CONFIG/PRIVATE_KEY/$PRIVATE_KEY}"
-
 OLD_CONFIG=$(cat wg0.conf)
 
 if [ ! -f wg0.conf ] || [ "$OLD_CONFIG" != "$NEW_CONFIG" ]
@@ -45,10 +31,40 @@ then
     chmod 600 wg0.conf
 fi
 
+
+
+NEW_EXPORTS="{% for peer_network in vault_cloud_vpn_networks %}
+{% if peer_network != main_network %}
+{{raid_path}}{{data_dir_name}}/local/{{peer_network}} {{vault_cloud_vpn_networks[peer_network].peer.allowedIPs}}(rw,async)
+{% endif %}
+{% endfor %}"
+
+OLD_EXPORTS=$(cat /etc/exports)
+
+if [ ! -f /etc/export ] || [ "$OLD_EXPORTS" != "$NEW_EXPORTS" ]
+then
+    echo "$NEW_EXPORTS" > /etc/exports
+    chmod 640 /etc/exports
+fi
+
+
+
+{% for peer_network in vault_cloud_vpn_networks %}
+{% if peer_network != main_network %}
+if ! grep -q '{{vault_cloud_vpn_networks[peer_network].peer.nfsServer}}' /etc/fstab ; then
+    echo '{{vault_cloud_vpn_networks[peer_network].peer.nfsServer}}:{{raid_path}}{{data_dir_name}}/local/{{peer_network}} {{raid_path}}{{data_dir_name}}/remote/{{peer_network}} nfs rw 0 0' >> /etc/fstab
+fi
+{% endif %}
+{% endfor %}
+
+
 wg-quick up /etc/wireguard/wg0.conf
 
 #iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #iptables -A FORWARD -i wg0 -j ACCEPT
+
+rc-service nfs start
+rc-service nfsmount start
 
 trap "echo TRAPed signal" HUP INT QUIT TERM
 
