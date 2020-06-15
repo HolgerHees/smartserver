95dc51f536b3b5b8a6d1fbcfc1a0a5b377dd93ae
Holger Hees <holger.hees@gmail.com>
2020-06-15 18:10:53 +0200

    activate loki logging

--- roles/alexa/templates/config/openhab.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    userpass: '{{vault_alexa_username}}:{{vault_alexa_web_password}}',
-    host: '{{server_domain}}',
-    port: {{vault_server_alexa_public_port}},
-    path: '/rest/items/',
-};
--- roles/grafana/templates/webui/grafana.js
+++ roles/grafana/templates/webui/grafana.js
@@ -1,1 +1,2 @@
-mx.Menu.getMainGroup('other').getSubGroup('states').addUrl('grafana',100, 'admin', '/grafana/d/server-health/server-health', '{i18n_Charts}', '{i18n_Grafana}', false);
+mx.Menu.getMainGroup('other').getSubGroup('states').addUrl('grafana_health',100, 'admin', '/grafana/d/server-health/server-health', '{i18n_Server Health}', '{i18n_Grafana}', false);
+mx.Menu.getMainGroup('other').getSubGroup('states').addUrl('grafana_logs',100, 'admin', '/grafana/d/logs/logs', '{i18n_Server Logs}', '{i18n_Grafana}', false);
