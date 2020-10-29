6eba362f0eb1263b75b85de8fae25b566e431af1
Holger Hees <holger.hees@gmail.com>
2020-10-29 10:02:39 +0100

    refactored userhandling

--- roles/openhab/templates/configuration.py
+++ roles/openhab/templates/configuration.py
@@ -13,7 +13,7 @@ allTelegramBots = [
 ]
 
 allTelegramAdminBots = [
-{% for username in userdata %}{% if userdata[username].webui == 'admin' and userdata[username].telegram is defined %}
-  {% if loop.index > 1 %},{% endif %}"{{userdata[username].telegram}}"
+{% for username in userdata %}{% if userdata[username].webui == 'admin' and userdata[username].telegram_bot is defined %}
+  {% if loop.index > 1 %},{% endif %}"{{userdata[username].telegram_bot}}"
 {% endif %}{% endfor %}
 ]
