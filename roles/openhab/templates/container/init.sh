4c3f418aea409fe7bfcba378287d4647f0f51994
Holger Hees <holger.hees@gmail.com>
2020-11-05 13:37:27 +0100

    improve habpanel handling

--- roles/openhab/templates/container/init.sh
+++ roles/openhab/templates/container/init.sh
@@ -8,11 +8,3 @@ if ! grep -q "log4j2.logger.jsr223Script" /openhab/userdata/etc/org.ops4j.pax.lo
     echo "log4j2.logger.jsr223Trigger.name = org.openhab.core.automation.internal.RuleEngineImpl" >> /openhab/userdata/etc/org.ops4j.pax.logging.cfg
     echo "log4j2.logger.jsr223Trigger.level = ERROR" >> /openhab/userdata/etc/org.ops4j.pax.logging.cfg
 fi 
-
-if test -f "/openhab/conf/habpanel.config"; then
-    mkdir -p /openhab/userdata/config/org/openhab/
-
-    ln -sf /openhab/conf/habpanel.config /openhab/userdata/config/org/openhab/habpanel.config
-
-    chown -R openhab:openhab /openhab/userdata/
-fi
