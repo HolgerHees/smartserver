4751e216dd362e938161d9affc581602d94ccd96
Holger Hees <holger.hees@gmail.com>
2020-11-22 13:25:10 +0100

    refactored backup handling

--- roles/deployment/templates/collect_backup.sh
+++ roles/deployment/templates/collect_backup.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-rm -rf {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/openhab/
-mkdir {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/openhab/
+rm -rf {{deployment_backup_path}}openhab/
+mkdir {{deployment_backup_path}}openhab/
 if [ -d "{{global_lib}}openhab/" ]; then
-    cp -r {{global_lib}}openhab/ {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/
+    cp -r {{global_lib}}openhab/ {{deployment_backup_path}}
 fi
 
-rm -rf {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/mysql/
-mkdir {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/mysql/
-FILE=$(ls -td {{local_backup_path}}mysql/nextcloud* 2>/dev/null | head -n 1)
+rm -rf {{deployment_backup_path}}mysql/
+mkdir {{deployment_backup_path}}mysql/
+FILE=$(ls -td {{backup_path}}mysql/nextcloud* 2>/dev/null | head -n 1)
 if [ ! -z $FILE ]; then
-    cp $FILE {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/mysql/
+    cp $FILE {{deployment_backup_path}}mysql/
 fi
 
-FILE=$(ls -td {{local_backup_path}}mysql/openhab* 2>/dev/null | head -n 1)
+FILE=$(ls -td {{backup_path}}mysql/openhab* 2>/dev/null | head -n 1)
 if [ ! -z $FILE ]; then
-    cp $FILE {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/mysql/
+    cp $FILE {{deployment_backup_path}}mysql/
 fi
 
-rm -rf {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/influxdb/
-mkdir {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/influxdb/
-FILE=$(ls -td {{local_backup_path}}influxdb/openhab* 2>/dev/null | head -n 1)
+rm -rf {{deployment_backup_path}}influxdb/
+mkdir {{deployment_backup_path}}influxdb/
+FILE=$(ls -td {{backup_path}}influxdb/openhab* 2>/dev/null | head -n 1)
 if [ ! -z $FILE ]; then
-    cp -r $FILE/ {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/influxdb/
+    cp -r $FILE/ {{deployment_backup_path}}influxdb/
 fi
-FILE=$(ls -td {{local_backup_path}}influxdb/opentsdb* 2>/dev/null | head -n 1)
+FILE=$(ls -td {{backup_path}}influxdb/opentsdb* 2>/dev/null | head -n 1)
 if [ ! -z $FILE ]; then
-    cp -r $FILE/ {{projects_path}}{{ansible_project_name}}/{{config_path}}backup/influxdb/
+    cp -r $FILE/ {{deployment_backup_path}}influxdb/
 fi
