f8e95cb56c85bde88dcddc11d978b1371fad5742
Holger Hees <holger.hees@gmail.com>
2020-12-03 18:33:01 +0100

    fix wireguard_cloud with empty peers

--- roles/wireguard_cloud/templates/util/wg-startup.sh
+++ roles/wireguard_cloud/templates/util/wg-startup.sh
@@ -10,7 +10,106 @@ source ./peers
 #https://www.heise.de/ct/artikel/NFSv4-unter-Linux-221582.html?seite=all
 # check cat /proc/net/rpc/auth.unix.ip/content
 
-mountShares()
+stop()
+{
+    echo "SIGTERM caught, shutting down..."
+    
+    echo "unmount nfs shares"
+    for name in $peers
+    do
+        echo "unmount /cloud/remote/$name"
+        umount -f -l /cloud/remote/$name > /dev/null 2>&1
+    done
+
+    echo "terminating nfs process(es)"
+    /usr/sbin/exportfs -uav
+    /usr/sbin/rpc.nfsd 0
+    pid1=`pidof rpc.nfsd`
+    pid2=`pidof rpc.mountd`
+    #pid3=`pidof rpc.statd`
+    # For IPv6 bug:
+    #pid4=`pidof rpcbind`
+    #kill -TERM $pid1 > /dev/null 2>&1
+    kill -TERM $pid1 $pid2 > /dev/null 2>&1
+
+    echo "shutting down wireguard interface"
+    wg-quick down wg0
+
+    echo "done"
+    exit
+}
+
+startWireguard()
+{
+    ip link del dev wg0 > /dev/null 2>&1
+
+    #echo "exported folder"
+    #cat /etc/exports
+
+    echo "setting up wireguard interface"
+    wg-quick up ./wg0.conf
+    
+}
+
+exportLocalShares()
+{
+    if [ -n "$peers" ]
+    then
+        #echo "open nlockmgr ports"
+        #mount -t nfsd nfsd /proc/fs/nfsd
+        #echo 'fs.nfs.nlm_tcpport=32768' >> /etc/sysctl.conf
+        #echo 'fs.nfs.nlm_udpport=32768' >> /etc/sysctl.conf
+        #sysctl -p > /dev/null
+        
+        # Normally only required if v3 will be used
+        # But currently enabled to overcome an NFS bug around opening an IPv6 socket
+        #echo "starting rpcbind"
+        #/sbin/rpcbind -w
+        #echo "starting idmapd"
+        #/usr/sbin/rpc.idmapd
+        #echo "starting statd"
+        #/sbin/rpc.statd
+        ##-p 32765 -o 32766
+        
+        # Kerberos
+        #/usr/sbin/rpc.svcgssd -v
+        #/usr/sbin/rpc.gssd -v
+        
+        echo "starting nfs"
+        /usr/sbin/rpc.nfsd --debug --no-nfs-version 2 --no-nfs-version 3
+        #sleep 1
+        #/usr/sbin/rpc.nfsd 0
+        #sleep 1
+        #/usr/sbin/rpc.nfsd --debug --no-nfs-version 2
+        
+        echo "starting exportfs"
+        FS_RESULT=$(/usr/sbin/exportfs -arv)
+        if [ -z "$FS_RESULT" ]
+        then
+          echo "export validation failed"
+          exit 1
+        else
+          echo $FS_RESULT
+        fi
+        
+        #echo "starting mountd"
+        /usr/sbin/rpc.mountd --debug all --no-nfs-version 2 --no-nfs-version 3
+        # --exports-file /etc/exports
+
+        # check if nfc is running
+        pid=`pidof rpc.mountd`
+        if [ -z "$pid" ]; then
+          echo "startup of nfs failed"
+          exit 1
+        fi
+        
+        echo "...done"
+    else
+        echo "skipped local share exports"
+    fi
+}
+
+mountRemoteShares()
 {
     if [ -n "$peers" ]
     then
@@ -47,107 +146,17 @@ mountShares()
             fi
         done
     else
-        echo "skipped nfs mounts"
+        echo "skipped remote share mounts"
     fi
-}
-
-stop()
-{
-    echo "SIGTERM caught, shutting down..."
-    
-    echo "unmount nfs shares"
-    for name in $peers
-    do
-        echo "unmount /cloud/remote/$name"
-        umount -f -l /cloud/remote/$name > /dev/null 2>&1
-    done
-
-    echo "terminating nfs process(es)"
-    /usr/sbin/exportfs -uav
-    /usr/sbin/rpc.nfsd 0
-    pid1=`pidof rpc.nfsd`
-    pid2=`pidof rpc.mountd`
-    #pid3=`pidof rpc.statd`
-    # For IPv6 bug:
-    #pid4=`pidof rpcbind`
-    #kill -TERM $pid1 > /dev/null 2>&1
-    kill -TERM $pid1 $pid2 > /dev/null 2>&1
-
-    echo "shutting down wireguard interface"
-    wg-quick down wg0
-
-    echo "done"
-    exit
-}
-
-start()
-{
-    ip link del dev wg0 > /dev/null 2>&1
-
-    echo "starting container..."
-    
-    #echo "exported folder"
-    #cat /etc/exports
-
-    echo "setting up wireguard interface"
-    wg-quick up ./wg0.conf
-    
-    #echo "open nlockmgr ports"
-    #mount -t nfsd nfsd /proc/fs/nfsd
-    #echo 'fs.nfs.nlm_tcpport=32768' >> /etc/sysctl.conf
-    #echo 'fs.nfs.nlm_udpport=32768' >> /etc/sysctl.conf
-    #sysctl -p > /dev/null
-    
-    # Normally only required if v3 will be used
-    # But currently enabled to overcome an NFS bug around opening an IPv6 socket
-    #echo "starting rpcbind"
-    #/sbin/rpcbind -w
-    #echo "starting idmapd"
-    #/usr/sbin/rpc.idmapd
-    #echo "starting statd"
-    #/sbin/rpc.statd
-    ##-p 32765 -o 32766
-    
-    # Kerberos
-    #/usr/sbin/rpc.svcgssd -v
-    #/usr/sbin/rpc.gssd -v
-    
-    echo "starting nfs"
-    /usr/sbin/rpc.nfsd --debug --no-nfs-version 2 --no-nfs-version 3
-    #sleep 1
-    #/usr/sbin/rpc.nfsd 0
-    #sleep 1
-    #/usr/sbin/rpc.nfsd --debug --no-nfs-version 2
-    
-    echo "starting exportfs"
-    FS_RESULT=$(/usr/sbin/exportfs -arv)
-    if [ -z "$FS_RESULT" ]
-    then
-      echo "export validation failed"
-      exit 1
-    else
-      echo $FS_RESULT
-    fi
-    
-    #echo "starting mountd"
-    /usr/sbin/rpc.mountd --debug all --no-nfs-version 2 --no-nfs-version 3
-    # --exports-file /etc/exports
-
-    # check if nfc is running
-    pid=`pidof rpc.mountd`
-    if [ -z "$pid" ]; then
-      echo "startup of nfs failed"
-      exit 1
-    fi
-    
-    echo "...done"
 }
 
 trap "stop" SIGTERM SIGINT
 
-start
+startWireguard
 
-mountShares
+exportLocalShares
+
+mountRemoteShares
 
 # wait forever or until we get SIGTERM or SIGINT
 while :; do sleep 360 & wait; done
