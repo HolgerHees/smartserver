3bdd81205344d04bae98845fc45147d3ad7fb3db
Holger Hees <holger.hees@gmail.com>
2021-01-18 13:01:40 +0100

    refactored cloud setup

--- roles/wireguard_cloud/templates/gateway_init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/bin/nsenter -n -t $(/usr/bin/docker inspect --format {{'{{'}}.State.Pid{{'}}'}} {{container_name}}) /usr/sbin/ip route del default
-/usr/bin/nsenter -n -t $(/usr/bin/docker inspect --format {{'{{'}}.State.Pid{{'}}'}} {{container_name}}) /usr/sbin/ip route add default via {{gateway}}
