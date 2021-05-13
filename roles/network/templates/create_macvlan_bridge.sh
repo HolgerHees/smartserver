4cc6e99f774191ee19613dba4c5ebb6c4c48ed74
Holger Hees <holger.hees@gmail.com>
2021-05-13 11:50:44 +0200

    improved mobile wireguard setup

---
+++ roles/network/templates/create_macvlan_bridge.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if /usr/sbin/ethtool mac0 2>/dev/null | grep -q "Link detected"; then
+  ip link del mac0
+fi
+
+ip link add mac0 link {{main_network_interface}} type macvlan mode bridge
+ip addr add {{macvlan_range}} dev mac0
+ip link set mac0 up
