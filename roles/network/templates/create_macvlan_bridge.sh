6b9e29e78451656847c2cc6dfb9e5467a7ac5cfc
Holger Hees <holger.hees@gmail.com>
2021-05-15 11:01:59 +0200

    wireguiard mobile routing improvements

--- roles/network/templates/create_macvlan_bridge.sh
+++ roles/network/templates/create_macvlan_bridge.sh
@@ -4,6 +4,6 @@ if /usr/sbin/ethtool mac0 2>/dev/null | grep -q "Link detected"; then
   ip link del mac0
 fi
 
-ip link add mac0 link {{main_network_interface}} type macvlan mode bridge
-ip addr add {{macvlan_range}} dev mac0
+ip addr add  {{server_ip}}/32 dev mac0
 ip link set mac0 up
+ip route add {{macvlan_range}} dev mac0
