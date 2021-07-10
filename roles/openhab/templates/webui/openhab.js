1cc5f91c41bd6194fd593c7a0df8f84b700f8063
Holger Hees <holger.hees@gmail.com>
2021-07-10 14:45:44 +0200

    add openhab metrics webui menu entry

--- roles/openhab/templates/webui/openhab.js
+++ roles/openhab/templates/webui/openhab.js
@@ -1,5 +1,7 @@
 var subGroup = mx.Menu.getMainGroup('automation').addSubGroup('openhab', 100, '{i18n_Openhab}', 'openhab_logo.svg');
 
 subGroup.addUrl('basicui', '//openhab.{host}/basicui/app', 'user', 100, '{i18n_Control}', '{i18n_Basic UI}', false, 'openhab_basicui.svg');
-subGroup.addUrl('mainui', '//openhab.{host}/', 'admin', 110, '{i18n_Administration}', '{i18n_Main UI}', false, 'openhab_adminui.svg');
 subGroup.addUrl('habot', '//openhab.{host}/habot', 'user', 120, '{i18n_Chatbot}', '{i18n_Habot}', false, 'openhab_habot.svg');
+
+subGroup.addUrl('mainui', '//openhab.{host}/', 'admin', 210, '{i18n_Administration}', '{i18n_Main UI}', false, 'openhab_adminui.svg');
+subGroup.addUrl('metrics', '/grafana/d/openhab_metrics/openhab_metrics','admin', 220, '{i18n_Metrics}', '{i18n_Grafana}', false, 'grafana_logs.svg');
