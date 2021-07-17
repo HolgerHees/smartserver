2ee33d1bb8eda561343591007c66aecaab4e2937
Holger Hees <holger.hees@gmail.com>
2021-07-17 13:38:55 +0200

    add grafana subdomain

--- roles/grafana/templates/webui/grafana.js
+++ roles/grafana/templates/webui/grafana.js
@@ -1,2 +1,2 @@
-mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('grafana_logs', '/grafana/d/logs/logs?autofitpanels','admin', 100, '{i18n_Server Logs}', '{i18n_Grafana}', false, 'grafana_logs.svg');
-mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('grafana_ui', '/grafana/','admin', 210, '{i18n_Grafana}', '{i18n_Dashboards}', false, 'grafana_logo.svg');
+mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('grafana_logs', '//grafana.{host}/d/logs/logs?autofitpanels','admin', 100, '{i18n_Server Logs}', '{i18n_Grafana}', false, 'grafana_logs.svg');
+mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('grafana_ui', '//grafana.{host}/','admin', 210, '{i18n_Grafana}', '{i18n_Dashboards}', false, 'grafana_logo.svg');
--- roles/openhab/templates/webui/openhab.js
+++ roles/openhab/templates/webui/openhab.js
@@ -4,4 +4,4 @@ subGroup.addUrl('basicui', '//openhab.{host}/basicui/app', 'user', 100, '{i18n_C
 subGroup.addUrl('habot', '//openhab.{host}/habot', 'user', 120, '{i18n_Chatbot}', '{i18n_Habot}', false, 'openhab_habot.svg');
 
 subGroup.addUrl('mainui', '//openhab.{host}/', 'admin', 210, '{i18n_Administration}', '{i18n_Main UI}', false, 'openhab_adminui.svg');
-subGroup.addUrl('metrics', '/grafana/d/openhab_metrics/openhab_metrics','admin', 220, '{i18n_Metrics}', '{i18n_Grafana}', false, 'grafana_logs.svg');
+subGroup.addUrl('metrics', '//grafana.{host}/d/openhab_metrics/openhab_metrics','admin', 220, '{i18n_Metrics}', '{i18n_Grafana}', false, 'grafana_logs.svg');
