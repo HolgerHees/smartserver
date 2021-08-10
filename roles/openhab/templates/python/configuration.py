7d97b6f81e52876fa6551a5892bd08ed9ba252a1
Holger Hees <holger.hees@gmail.com>
2021-08-10 11:04:27 +0200

    removed jsr223 helper libraries

--- roles/openhab/templates/python/configuration.py
+++ roles/openhab/templates/python/configuration.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 LOG_PREFIX = "jsr223.jython"
 
-admin_email = "admin_email@some_domain.com"
-
-openhabHost = "localhost"
-openhabPort = "8080"# "8443"
-
 allTelegramBots = [
 {% for bot_name in vault_telegram_bots %}
   {% if loop.index > 1 %},{% endif %}"{{bot_name}}"
