039f72d9755e15b505768014bb895f6229e1c79c
Holger Hees <holger.hees@gmail.com>
2021-12-25 13:19:01 +0100

    letsencrypt cleanup

--- roles/apache/templates/letsencrypt/check.sh
+++ roles/apache/templates/letsencrypt/check.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/bash
 
-. /opt/helper/_config
+DOMAINS=`grep -ohP "ServerName\s*\K(.*){{server_domain}}" {{global_etc}}apache2/_.ansible.vhost.d/*.conf`
+UNIQUE_DOMAINS=`echo $DOMAINS | tr ' ' '\n' | sort -u | tr '\n' ' '| xargs | sed -e 's/ / -d /g'`
 
-COLLECT_CMD=$(printf 'grep -ohP "ServerName\s*\K(.*)%s" %sapache2/_.ansible.vhost.d/*.conf' $SERVER_DOMAIN $ETC_PATH)
-COLLECTED_DOMAINS=`eval $COLLECT_CMD`
-
-UNIQUE_DOMAINS=`echo $COLLECTED_DOMAINS | tr ' ' '\n' | sort -u | tr '\n' ' '| xargs | sed -e 's/ / -d /g'`
-
-LETSENCRYPT_CMD=$(printf 'docker exec apache2 sh -c "certbot certonly --webroot -w %sletsencrypt --dry-run --preferred-challenges http --agree-tos --email %s -d %s"' $HTDOCS_PATH $SERVER_EMAIL "$UNIQUE_DOMAINS")
+LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w /dataDisk/htdocs/letsencrypt --dry-run --preferred-challenges http --agree-tos --email holger.hees@gmail.com -d $UNIQUE_DOMAINS\""
 
 echo $LETSENCRYPT_CMD
 
