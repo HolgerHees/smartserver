28685926e69a975ce35e63f8650b4cf4f4f8f89c
Holger Hees <holger.hees@gmail.com>
2022-01-17 11:20:36 +0100

    add custom apache error pages to allow origin headers

--- roles/apache/templates/letsencrypt/check.sh
+++ roles/apache/templates/letsencrypt/check.sh
@@ -3,7 +3,7 @@
 DOMAINS=`grep -ohP "ServerName\s*\K(.*){{server_domain}}" {{global_etc}}apache2/_.ansible.vhost.d/*.conf`
 UNIQUE_DOMAINS=`echo $DOMAINS | tr ' ' '\n' | sort -u | tr '\n' ' '| xargs | sed -e 's/ / -d /g'`
 
-LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w /dataDisk/htdocs/letsencrypt --dry-run --preferred-challenges http --agree-tos --email holger.hees@gmail.com -d $UNIQUE_DOMAINS\""
+LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w {{htdocs_path}}letsencrypt --dry-run --preferred-challenges http --agree-tos --email {{vault_root_email}} -d $UNIQUE_DOMAINS\""
 
 echo $LETSENCRYPT_CMD
 
