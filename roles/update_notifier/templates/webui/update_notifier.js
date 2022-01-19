bcc42e007f77953c98aa01f61c0da95cf1116909
Holger Hees <holger.hees@gmail.com>
2022-01-19 19:35:04 +0100

    rename update_notifier menu and improved nextcloud app updater

--- roles/update_notifier/templates/webui/update_notifier.js
+++ roles/update_notifier/templates/webui/update_notifier.js
@@ -1,1 +1,1 @@
-mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('update_notifier', '/update_notifier/', 'admin', 310, '{i18n_Updates}', '{i18n_Software updates}', false, "update_notifier_logo.svg");
+mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('update_notifier', '/update_notifier/', 'admin', 310, '{i18n_Software}', '{i18n_Software status}', false, "update_notifier_logo.svg");
