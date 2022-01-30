3baae440356a6bf227687a034b9cddf285de6b27
Holger Hees <holger.hees@gmail.com>
2022-01-30 14:04:56 +0100

    revoke git token

--- roles/ci_service/templates/opt/ci_service/config/config.py
+++ roles/ci_service/templates/opt/ci_service/config/config.py
@@ -1,13 +1,13 @@
-server_host = "smartmarvin.de"
+server_host = "{{server_domain}}"
 
-status_file = "/dataDisk/tmp/ci_service.status"
+status_file = "{{global_tmp}}ci_service.status"
 
-lib_dir = "/dataDisk/var/lib/ci/"
-log_dir = "/dataDisk/var/log/ci/"
-build_dir = "/dataDisk/build/"
-repository_dir = "/dataDisk/build/ci_job"
+lib_dir = "{{global_lib}}ci/"
+log_dir = "{{global_log}}ci/"
+build_dir = "{{global_build}}"
+repository_dir = "{{global_build}}ci_job"
 
-repository_url = "https://github.com/HolgerHees/smartserver.git"
-access_token = "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"
+repository_url = "{{vault_deployment_config_git}}"
+access_token = "{{vault_deployment_token_git if vault_deployment_token_git != 'None' else ''}}"
 
-daemon_ip = "172.30.2.1"
+daemon_ip = "{{update_daemon_ip}}"
