6fc6e3d7de7e605c529ce8f2e48861d3b66cfb48
Holger Hees <holger.hees@gmail.com>
2022-02-18 15:57:52 +0100

    changed openhab i18n

--- roles/openhab/templates/webui/openhab.js
+++ roles/openhab/templates/webui/openhab.js
@@ -1,6 +1,6 @@
 var subGroup = mx.Menu.getMainGroup('automation').addSubGroup('openhab', 100, '{i18n_Openhab}', 'openhab_logo.svg');
 
-subGroup.addUrl('basicui', '//openhab.{host}/basicui/app', 'user', 100, '{i18n_Control}', '{i18n_Basic UI}', false, 'openhab_basicui.svg');
+subGroup.addUrl('basicui', '//openhab.{host}/basicui/app', 'user', 100, '{i18n_Homecontrol}', '{i18n_Basic UI}', false, 'openhab_basicui.svg');
 subGroup.addUrl('habot', '//openhab.{host}/habot', 'user', 120, '{i18n_Chatbot}', '{i18n_Habot}', false, 'openhab_habot.svg');
 
 subGroup.addUrl('mainui', '//openhab.{host}/', 'admin', 210, '{i18n_Administration}', '{i18n_Main UI}', false, 'openhab_adminui.svg');
