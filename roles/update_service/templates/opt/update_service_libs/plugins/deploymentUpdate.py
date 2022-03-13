9c283ac980cbd4ca3bb7dfa30271aae95d31954f
Holger Hees <holger.hees@gmail.com>
2022-03-13 12:01:17 +0100

    preperations for update messages

--- roles/update_service/templates/opt/update_service_libs/plugins/deploymentUpdate.py
+++ roles/update_service/templates/opt/update_service_libs/plugins/deploymentUpdate.py
@@ -25,6 +25,17 @@ class DeploymentUpdate:
                 except JSONDecodeError:
                     pass
       
+    def filterPath( self, flag, path, deployment_mtime, filtered_lines ):
+        if flag != "D":
+            file_stat = os.stat("{}/{}".format(self.config.deployment_directory,path))
+            file_mtime = file_stat.st_mtime
+            
+            if file_mtime > deployment_mtime:
+                if path not in filtered_lines or flag == "A":
+                    filtered_lines[path] = {"flag": flag, "path": path}
+                    return True
+        return False
+
     def process(self, update_time):
         smartserver_code = None
         smartserver_pull = None
@@ -76,47 +87,79 @@ class DeploymentUpdate:
                 smartserver_code = "uncommitted_changes"
                 
             last_deployment = datetime.fromtimestamp(deployment_mtime, tz=timezone.utc)
-            #last_deployment = "2020-01-20 14:02:00.651984+00:00"
+            last_deployment = datetime.strptime("2022-03-13 00:00:00 +0100","%Y-%m-%d %H:%M:%S %z")
+            
             #print( " ".join([ "git", "-C", self.config.deployment_directory, "rev-list", "-1", "--before", str(last_deployment), "origin/master" ]))
             result = command.exec([ "git", "rev-list", "-1", "--before", str(last_deployment), "origin/master" ], cwd=self.config.deployment_directory )
             ref = result.stdout.decode("utf-8").strip()
             
             #print( " ".join([ "git", "-C", self.config.deployment_directory, "diff-index", "--name-status", ref ]))
-            result = command.exec([ "git", "diff-index", "--name-status", ref ], cwd=self.config.deployment_directory )
+            #result = command.exec([ "git", "diff-index", "--name-status", ref ], cwd=self.config.deployment_directory )
+            #committed_changes = result.stdout.decode("utf-8").strip().split("\n")
+
+            # prepare commit messages
+            result = command.exec([ "git", "log", "--name-status", "--date=iso", str(ref) +  "..origin/master" ], cwd=self.config.deployment_directory )
             committed_changes = result.stdout.decode("utf-8").strip().split("\n")
             
-            #result = command.exec([ "git", "log", "--date=iso", "--pretty=%ad%x09%s", str(ref) +  "..origin/master" ], cwd=self.config.deployment_directory )
-            #commit_messages = result.stdout.decode("utf-8").strip().split("\n")
-            commit_lines = []
-            #for line in commit_messages:
-            #    data = line.split("\t",1)
-            #    print(data[0])
-            #    datetimeStr = data[0]#"{}{}".format(data[0][0:-3],data[0][-2:])
-            #    commit_datetime = datetime.strptime(datetimeStr,"%Y-%m-%d %H:%M:%S %z")
-            #    if commit_datetime > last_deployment:
-            #        commit_lines.append(data[1])
+            commits = {}
+            current_commit = None
+            current_date = None
+            current_message = []
+            current_files = []
+            for line in committed_changes:
+                if len(line) == 0:
+                    continue
+
+                if len(line) > 6 and line[:6] == "commit":
+                    if current_commit is not None:
+                        commits[current_commit] = {"date": current_date, "messages": current_message, "files": current_files }
+                    current_commit = line[6:].strip().split(" ",1)[0]
+                    current_date = None
+                    current_message = []
+                    current_files = []
+                    continue
+                elif current_commit is None:
+                    continue
                 
+                if len(line) > 5 and line[:5] == "Date:":
+                    current_date = line[5:].strip()
+                    continue
+                elif current_date is None:
+                    continue
+                                
+                if line[0] == " ":
+                    current_message.append(line.strip())
+                    continue
+
+                current_files.append( line.split("\t") )
+                
+            #print(commits)
+            
+            filtered_lines = {}
+            filtered_commit_messages = []
+            for commit in commits:
+                for file in commits[commit]["files"]:
+                    flag, path = file
+                    
+                    success = self.filterPath( flag, path, deployment_mtime, filtered_lines )
+                    if success:
+                        date = "{}T{}.000000{}:{}".format(commits[commit]["date"][:10],commits[commit]["date"][11:19],commits[commit]["date"][20:23],commits[commit]["date"][23:])
+                        filtered_commit_messages.append({"date": date, "message": "\n".join(commits[commit]["messages"]) })
+
+            #print(commits)
+            #print(filtered_commit_messages)
+            #print(filtered_lines)
             #print(last_deployment)
             #print(commit_lines)
             #print(committed_changes)
 
-            lines = uncommitted_changes + committed_changes
-            lines = [ele.split("\t") for ele in lines]
-            
-            filtered_lines = {}
+            lines = [ele.split("\t") for ele in uncommitted_changes]
             for line in lines:
                 if len(line) == 1:
                     continue
-                  
                 flag, path = line
-                
-                if flag != "D":
-                    file_stat = os.stat("{}/{}".format(self.config.deployment_directory,path))
-                    file_mtime = file_stat.st_mtime
-                    
-                    if file_mtime > deployment_mtime:
-                        if path not in filtered_lines or flag == "A":
-                            filtered_lines[path] = {"flag": flag, "path": path}
+                self.filterPath( flag, path, deployment_mtime, filtered_lines )
+
             filtered_lines = dict(sorted(filtered_lines.items()))
                             
             files = glob.glob("{}/**/**/*".format(config.deployment_config_path), recursive = True)
@@ -131,5 +174,4 @@ class DeploymentUpdate:
             
             smartserver_changes = lines
             
-        return smartserver_code, smartserver_pull, smartserver_changes, commit_lines
-
+        return smartserver_code, smartserver_pull, smartserver_changes, filtered_commit_messages
