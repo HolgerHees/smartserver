59dc6897197b3b0875fb64bc18c31b559071e7ae
Holger Hees <holger.hees@gmail.com>
2022-04-03 12:05:16 +0200

    preperations for multiple networks

--- roles/librenms/templates/etc/librenms/config.php
+++ roles/librenms/templates/etc/librenms/config.php
@@ -1,11 +1,43 @@
 <?php
+$config['snmp']['community'] = array();
+$config['nets'] = array("{{intern_networks | join('\", \"')}}");
 
-$config['snmp']['community'] = array('public');
-
-$config['nets'][] = "192.168.0.0/24";
-
-
-$config['discovery_modules']['discovery-arp'] = true;
-
-$config['discovery_modules']['ports-stack'] = false;
-$config['discovery_modules']['cisco-vrf-lite'] = false;
+/*$config['discovery_modules']['os']                   = false;
+$config['discovery_modules']['ports']                = false;
+$config['discovery_modules']['ports-stack']          = false;
+$config['discovery_modules']['entity-physical']      = false;
+$config['discovery_modules']['entity-state']         = false;
+$config['discovery_modules']['processors']           = false;
+$config['discovery_modules']['mempools']             = false;
+$config['discovery_modules']['cisco-vrf-lite']       = false;
+$config['discovery_modules']['cisco-mac-accounting'] = false;
+$config['discovery_modules']['cisco-pw']             = false;
+$config['discovery_modules']['vrf']                  = false;
+$config['discovery_modules']['cisco-cef']            = false;
+$config['discovery_modules']['cisco-sla']            = false;
+$config['discovery_modules']['cisco-cbqos']          = false;
+$config['discovery_modules']['cisco-otv']            = false;
+$config['discovery_modules']['ipv4-addresses']       = false;
+$config['discovery_modules']['ipv6-addresses']       = false;
+$config['discovery_modules']['route']                = false;
+$config['discovery_modules']['sensors']              = false;
+$config['discovery_modules']['storage']              = false;
+$config['discovery_modules']['hr-device']            = false;
+$config['discovery_modules']['discovery-protocols']  = false;
+$config['discovery_modules']['arp-table']            = false;
+$config['discovery_modules']['discovery-arp']        = false;
+$config['discovery_modules']['junose-atm-vp']        = false;
+$config['discovery_modules']['bgp-peers']            = false;
+$config['discovery_modules']['vlans']                = false;
+$config['discovery_modules']['vmware-vminfo']        = false;
+$config['discovery_modules']['libvirt-vminfo']       = false;
+$config['discovery_modules']['toner']                = false;
+$config['discovery_modules']['ucd-diskio']           = false;
+$config['discovery_modules']['applications']         = false;
+$config['discovery_modules']['services']             = false;
+$config['discovery_modules']['stp']                  = false;
+$config['discovery_modules']['ntp']                  = false;
+$config['discovery_modules']['loadbalancers']        = false;
+$config['discovery_modules']['mef']                  = false;
+$config['discovery_modules']['wireless']             = false;
+$config['discovery_modules']['fdb-table']            = false;*/
