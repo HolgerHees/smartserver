e71437807044e5ee58b75c7a056ab8e292ab8be1
Holger Hees <holger.hees@gmail.com>
2022-04-20 12:51:50 +0200

    fix letsencrypt

--- roles/apache/templates/letsencrypt/check.sh
+++ roles/apache/templates/letsencrypt/check.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-DOMAINS=`grep -ohP "ServerName\s*\K(.*){{server_domain}}" {{global_etc}}apache2/_.ansible.vhost.d/*.conf`
+DOMAINS=`grep -ohP "ServerName\s*\K(.+)\.{{server_domain}}" {{global_etc}}apache2/_.ansible.vhost.d/*.conf`
 UNIQUE_DOMAINS=`echo $DOMAINS | tr ' ' '\n' | sort -u | tr '\n' ' '| xargs | sed -e 's/ / -d /g'`
 
-LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w {{htdocs_path}}letsencrypt --dry-run --preferred-challenges http --agree-tos --email {{root_email}} -d $UNIQUE_DOMAINS\""
+LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w {{htdocs_path}}letsencrypt --dry-run --preferred-challenges http --agree-tos --email {{root_email}} -d {{server_domain}} -d $UNIQUE_DOMAINS\""
 
 echo $LETSENCRYPT_CMD
 
