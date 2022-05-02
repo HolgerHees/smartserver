576bd0313f1f296290cacc52f9bdf5c1802fa531
Holger Hees <holger.hees@gmail.com>
2022-05-02 19:47:45 +0200

    improved netdata theme support

--- roles/netdata/templates/webui/netdata.js
+++ roles/netdata/templates/webui/netdata.js
@@ -1,4 +1,14 @@
-mx.Menu.getMainGroup('admin').getSubGroup('system').addUrl('netdata','//netdata.{host}/', 'admin', 110, '{i18n_Server State}', '{i18n_Netdata}', false, "netdata_logo.svg");
+mx.Netdata = (function( ret ) {
+  ret.applyTheme = function(url)
+  {
+      url += url.includes("?") ? '&' : '?';
+      url += "theme=" + ( mx.Page.isDarkTheme() ? "slate": "white" );
+      return url;
+  }
+  return ret;
+})( mx.Netdata || {} ); 
+
+mx.Menu.getMainGroup('admin').getSubGroup('system').addUrl('netdata',{ "url": '//netdata.{host}/', "callback": mx.Netdata.applyTheme }, 'admin', 110, '{i18n_Server State}', '{i18n_Netdata}', false, "netdata_logo.svg");
 
 mx.Alarms = (function( ret ) {
     var buttonSelector;
