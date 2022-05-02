0de3e4f42d34b0cb22b408fcb7ee99f101ee729a
Holger Hees <holger.hees@gmail.com>
2022-05-02 20:11:03 +0200

    fix system service

--- roles/system_service/templates/opt/system_service_libs/lib/dto/device.py
+++ roles/system_service/templates/opt/system_service_libs/lib/dto/device.py
@@ -196,10 +196,11 @@ class Device(Changeable):
     def disableHopConnection(self, type, target_mac, target_interface):
         _connections = list(filter(lambda c: c.getType() == type and c.getTargetMAC() == target_mac and c.getTargetInterface() == target_interface, self.hop_connections ))
         if len(_connections) > 0:
-            _connections[0].setEnabled(False)
-            
-            target_device = self.cache.getUnlockedDevice(target_mac)
-            self._markAsChanged("connection", "disable connection to {}:{}".format(target_device if target_device else target_mac,vlan))    
+            for for connection in _connections:
+                connection.setEnabled(False)
+                
+                target_device = self.cache.getUnlockedDevice(target_mac)
+                self._markAsChanged("connection", "disable connection to {}:{}".format(target_device if target_device else target_mac,",".join(connection.getVLANs())))    
 
     def cleanDisabledHobConnections(self, type, event_callback):
         _connections = list(filter(lambda c: c.getType() == type and not c.isEnabled(), self.hop_connections ))
@@ -210,7 +211,7 @@ class Device(Changeable):
             
             target_mac = _connection.getTargetMAC()
             target_device = self.cache.getUnlockedDevice(target_mac)
-            self._markAsChanged("connection", "remove disabled connection from {}:{}".format(target_device if target_device else target_mac, vlan))
+            self._markAsChanged("connection", "remove disabled connection from {}:{}".format(target_device if target_device else target_mac, ",".join(connection.getVLANs())))
 
     def getHopConnections(self):
         return list(self.hop_connections)
