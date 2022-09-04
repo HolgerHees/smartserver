140b4f9f0fb84f1488c9a6fc8b66a1de51c49bdf
Holger Hees <holger.hees@gmail.com>
2022-09-04 11:18:13 +0200

    improved openhab config

--- roles/openhab/templates/python/configuration.py
+++ roles/openhab/templates/python/configuration.py
@@ -3,6 +3,6 @@ LOG_PREFIX = "jsr223.jython"
 
 userConfigs = {
 {% for username in userdata %}{% if userdata[username].openhab is defined %}
-  {% if loop.index > 1 %},{% endif %}"{{username}}": { "state_item": {% if userdata[username].openhab.state_item is defined %}"{{userdata[username].openhab.state_item}}"{% else %}None{% endif %}, "notification_config": {% if userdata[username].openhab.notification_config is defined %}[ '{{userdata[username].openhab.notification_config | join("','") }}' ]{% else %}None{% endif %}, "is_admin": {{ 'True' if 'admin' in userdata[username].groups else 'False'}} }
+  {% if loop.index > 1 %},{% endif %}"{{username}}": { "state_item": {% if userdata[username].openhab.state_item is defined %}"{{userdata[username].openhab.state_item}}"{% else %}None{% endif %}, "notification_config": {% if userdata[username].openhab.notification_config is defined %}[ '{{userdata[username].openhab.notification_config | join("','") }}' ]{% else %}None{% endif %}, "is_admin": {{ 'True' if 'admin' in userdata[username].groups else 'False'}}, "name": "{{userdata[username].name}}", "email": "{{userdata[username].email}}" }
 {% endif %}{% endfor %}
 }
