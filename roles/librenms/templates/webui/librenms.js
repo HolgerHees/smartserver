0ee39150e5a56e1455bc93bd1c94bb542ee98467
Holger Hees <holger.hees@gmail.com>
2022-10-24 18:28:56 +0200

    fix librenms url

--- roles/librenms/templates/webui/librenms.js
+++ roles/librenms/templates/webui/librenms.js
@@ -1,1 +1,1 @@
-mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('librenms', '//librenms.{host}/','admin', 350, '{i18n_SNMP}', '{i18n_LibreNMS}', 'librenms_logo.svg', false);
+mx.Menu.getMainGroup('admin').getSubGroup('tools').addUrl('librenms', '//librenms.{host}/devices','admin', 350, '{i18n_SNMP}', '{i18n_LibreNMS}', 'librenms_logo.svg', false);
