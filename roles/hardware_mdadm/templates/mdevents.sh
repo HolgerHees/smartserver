1e951e7bbd2d2ccf3e488c133d0e19d30a041f7b
Holger Hees <holger.hees@gmail.com>
2022-12-01 20:36:55 +0100

    Improved raid check

---
+++ roles/hardware_mdadm/templates/mdevents.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+event=$1
+device=$2
+
+state=$(cat /proc/mdstat)
+
+if [ "$event" = "Fail" ]; then
+    printf "Subject: RAID: A failure has been detected on $device\n\n$state" | sendmail root
+elif [ $event == "FailSpare" ]; then
+    printf "Subject: RAID: A failure has been detected on spare $device\n\n$state" | sendmail root
+elif [ $event == "DegradedArray" ]; then
+    printf "Subject: RAID: A degraded array has been detected on $device\n\n$state" | sendmail root
+elif [ $event == "TestMessage" ]; then
+    printf "Subject: RAID: A test message for $device\n\n$state" | sendmail root
+fi
