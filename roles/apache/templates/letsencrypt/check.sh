44ac25e1467f8a34bd0f3ceac3eeac0bf594af4b
Holger Hees <holger.hees@gmail.com>
2023-02-03 17:21:08 +0100

    apache improvements

--- roles/apache/templates/letsencrypt/check.sh
+++ roles/apache/templates/letsencrypt/check.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/bash
 
-DOMAINS=`grep -ohP "ServerName\s*\K(.+)\.{{server_domain}}" {{global_etc}}apache2/_.ansible.vhost.d/*.conf`
+EMAIL="{{root_email}}"
+SERVER="{{server_domain}}"
+HTDOCS="{{htdocs_path}}"
+ETC="{{global_etc}}"
+DOMAINS=`grep -ohP "ServerName\s*\K(.+)\.$SERVER" ${ETC}apache2/_.ansible.vhost.d/*.conf`
+#UNIQUE_DOMAINS=`echo $DOMAINS | tr ' ' '\n' | sort -u | tr '\n' ' '| xargs | sed -e 's/ / -d /g'`
 UNIQUE_DOMAINS=`echo $DOMAINS | tr ' ' '\n' | sort -u | tr '\n' ' '| xargs | sed -e 's/ / -d /g'`
 
-LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w {{htdocs_path}}_public --dry-run --preferred-challenges http --agree-tos --email {{root_email}} -d {{server_domain}} -d $UNIQUE_DOMAINS\""
+if [ $# -gt 0 ] & [ "$1" == "update" ]
+then
+  LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w ${HTDOCS}_public --preferred-challenges http --agree-tos --email $EMAIL --cert-name $SERVER -d $SERVER -d $UNIQUE_DOMAINS\""
 
-echo $LETSENCRYPT_CMD
+  printf "\n\n"
 
-printf "\n\n"
+  eval $LETSENCRYPT_CMD
+else
+  LETSENCRYPT_CMD="docker exec apache2 sh -c \"certbot certonly --webroot -w ${HTDOCS}_public --dry-run --preferred-challenges http --agree-tos --email $EMAIL --cert-name $SERVER -d $SERVER -d $UNIQUE_DOMAINS\""
 
-eval $LETSENCRYPT_CMD
+  echo $LETSENCRYPT_CMD
+
+  printf "\n\n"
+
+  eval $LETSENCRYPT_CMD
+fi
