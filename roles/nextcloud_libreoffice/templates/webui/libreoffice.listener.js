294b69da11e6c3c97741b1d7d1f737e9d52c0d0f
Holger Hees <holger.hees@gmail.com>
2023-03-04 08:22:17 +0100

    improved libreoffice css

--- roles/nextcloud_libreoffice/templates/webui/libreoffice.listener.js
+++ roles/nextcloud_libreoffice/templates/webui/libreoffice.listener.js
@@ -1,55 +1,14 @@
 var mxTheme = document.cookie.split( ';' ).map( function( x ) { return x.trim().split( '=' ); } ).reduce( function( a, b ) { a[ b[ 0 ] ] = b[ 1 ]; return a; }, {} )[ "theme" ];
 
-var css = '';
-
-if( mxTheme == 'light' )
-{
-    css += `
-    nav:not(.hasnotebookbar).main-nav {
-        background-color: #1976D2 !important;
-    }
-    nav:not(.hasnotebookbar) #main-menu > li > a {
-        color: white !important;
-        border: none !important;
-    }
-    nav:not(.hasnotebookbar) #main-menu > li > a.highlighted,
-    nav:not(.hasnotebookbar) #main-menu > li > a:active,
-    nav:not(.hasnotebookbar) #main-menu > li > a:hover,
-    #closebuttonwrapper:hover,
-    nav:not(.hasnotebookbar) > #icon-nextcloud-sidebar.icon-nextcloud-sidebar:hover {
-        background-color: #0966C2 !important;
-    }
-    nav:not(.hasnotebookbar) #menu-last-mod > a span, nav:not(.hasnotebookbar) #menu-last-mod > a span:hover, #document-name-input {
-        color: #3c4043 !important;
-    }
-   `;
-}
-else if( mxTheme == 'dark' )
-{
-    css += `
-    nav:not(.hasnotebookbar).main-nav {
-        background-color: #1976d24D !important;
-    }
-    nav:not(.hasnotebookbar) #main-menu > li > a {
-        color: #202124 !important;
-        border: none !important;
-    }
-    nav:not(.hasnotebookbar) #main-menu > li > a.highlighted,
-    nav:not(.hasnotebookbar) #main-menu > li > a:active,
-    nav:not(.hasnotebookbar) #main-menu > li > a:hover,
-    #closebuttonwrapper:hover,
-    nav:not(.hasnotebookbar) > #icon-nextcloud-sidebar.icon-nextcloud-sidebar:hover {
-        background-color: #0966C24D !important;
-    }
-    nav:not(.hasnotebookbar) #menu-last-mod > a span, nav:not(.hasnotebookbar) #menu-last-mod > a span:hover, #document-name-input {
-        color: #fff !important;
-    }
-   `;
-}
-
-css += `
+var css = `
     nav:not(.hasnotebookbar).main-nav {
         height: 53px !important;
+    }
+    .lo-menu > li a {
+        color: #3c4043
+    }
+    .lo-menu > li .disabled {
+        opacity: 0.4;
     }
 `;
 var element = document.createElement('style');
