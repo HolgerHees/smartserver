d3b725c50908438e13cc1e193a108ab350ee2e57
HolgerHees <holger.hees@gmail.com>
2024-08-04 17:01:30 +0200

    cleanups

--- roles/ci_service/templates/config.py
+++ roles/ci_service/templates/config.py
@@ -1,10 +1,10 @@
 server_host = "{{server_domain}}"
 
-status_file = "/tmp/ci_service/ci_service.status"
+status_file = "{{global_tmp}}ci_service.status"
 
-lib_dir = "/var/lib/ci_service/"
-log_dir = "/var/log/ci_service/"
-repository_dir = "/opt/ci_job/"
+lib_dir = "{{global_lib}}ci_service/"
+log_dir = "{{global_log}}ci_service/"
+repository_dir = "{{global_build}}ci_job/"
 
 repository_url = "{{deployment_config_git}}"
 auth_token = "{{github_auth_token if github_auth_token is defined else ''}}"
