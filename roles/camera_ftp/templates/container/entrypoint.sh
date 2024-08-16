b255fcd5a0369416ad632cc89c50c6d90484369f
HolgerHees <holger.hees@gmail.com>
2024-08-16 14:23:21 +0200

    simplified vsftpd config

--- roles/camera_ftp/templates/container/entrypoint.sh
+++ roles/camera_ftp/templates/container/entrypoint.sh
@@ -12,44 +12,7 @@ chown $FTP_USER:$FTP_USER /home/upload/ -R
 
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd > /dev/null 2>&1
 
-#cp /etc/vsftpd/vsftpd.conf_or /etc/vsftpd/vsftpd.conf
-#echo "log_ftp_protocol=YES" >> /etc/vsftpd/vsftpd.conf
-
-if [[ "$PASV_ENABLE" == "YES" ]]; then
-  echo "PASV is enabled"
-  echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
-  echo "pasv_max_port=$PASV_MAX" >> /etc/vsftpd/vsftpd.conf
-  echo "pasv_min_port=$PASV_MIN" >> /etc/vsftpd/vsftpd.conf
-  echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftpd/vsftpd.conf
-else
-  echo "pasv_enable=NO" >> /etc/vsftpd/vsftpd.conf
-fi
-
-if [[ "$ONLY_UPLOAD" == "YES" ]]; then
-  echo "Only uploads allowed"
-  echo "download_enable=NO" >> /etc/vsftpd/vsftpd.conf
-  #echo "ftpd_banner=Welcome to FTP Server. Note: this FTP server only accepts upload." >> /etc/vsftpd/vsftpd.conf
-elif [[ "$ONLY_DOWNLOAD" == "YES" ]]; then
-  echo "Only downloads allowed"
-  #echo "ftpd_banner=Welcome to FTP Server. Note: this FTP server only accepts download." >> /etc/vsftpd/vsftpd.conf
-  sed -i 's/write_enable=YES/write_enable=NO/g' /etc/vsftpd/vsftpd.conf
-else
-  echo "ftpd_banner=Welcome to FTP Server" >> /etc/vsftpd/vsftpd.conf
-fi
-
-echo "local_umask=$UMASK" >> /etc/vsftpd/vsftpd.conf
-
 export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
-
-#echo "$LOG_FILE" >> /etc/logrotate.d/vsftpd
-#echo "{" >> /etc/logrotate.d/vsftpd
-#echo "	create 640 root adm" >> /etc/logrotate.d/vsftpd
-#echo "	# ftpd doesn't handle SIGHUP properly" >> /etc/logrotate.d/vsftpd
-#echo "	missingok" >> /etc/logrotate.d/vsftpd
-#echo "	notifempty" >> /etc/logrotate.d/vsftpd
-#echo "	rotate 0" >> /etc/logrotate.d/vsftpd
-#echo "	daily" >> /etc/logrotate.d/vsftpd
-#echo "}" >> /etc/logrotate.d/vsftpd
 
 /bin/ln -sf /proc/$$/fd/1 $LOG_FILE
 #/bin/ln -sf /proc/1/fd/1 $LOG_FILE
