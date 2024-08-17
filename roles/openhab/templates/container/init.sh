79ce44d0be3971f469d3efe7f5db8346309f09b8
HolgerHees <holger.hees@gmail.com>
2024-08-17 18:32:28 +0200

    change jetty loglevel in openhab

--- roles/openhab/templates/container/init.sh
+++ roles/openhab/templates/container/init.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-#if ! grep -q "name=\"org.openhab.core.automation.internal.RuleEngineImpl\"" /openhab/userdata/etc/log4j2.xml; then
-#    sed -i 's/<\/Loggers>/\n\t\t<Logger level="ERROR" name="org.openhab.core.automation.internal.RuleEngineImpl"\/>\n\t<\/Loggers>/g' /openhab/userdata/etc/log4j2.xml
-#fi 
-    
-#if ! grep -q "name=\"org.openhab.core.automation\"" /openhab/userdata/etc/log4j2.xml; then
-#    sed -i 's/<\/Loggers>/\n\t\t<Logger level="DEBUG" name="org.openhab.core.automation"\/>\n\t<\/Loggers>/g' /openhab/userdata/etc/log4j2.xml
-#fi 
-
 if ! grep -q "name=\"jsr223.jython\"" /openhab/userdata/etc/log4j2.xml; then
     sed -i 's/<\/Loggers>/\n\t\t<Logger level="DEBUG" name="jsr223.jython"\/>\n\t<\/Loggers>/g' /openhab/userdata/etc/log4j2.xml
 fi 
+
+if ! grep -q "name=\"org.eclipse.jetty.util.ssl.SslContextFactory.config\"" /openhab/userdata/etc/log4j2.xml; then
+    # needed to hide warning about "Weak cipher suite" in samsung tv binding
+    # https://github.com/openhab/openhab-addons/issues/17273
+    # https://community.openhab.org/t/weak-cipher-suite-warnings/157840
+    sed -i 's/<\/Loggers>/\n\t\t<Logger level="ERROR" name="org.eclipse.jetty.util.ssl.SslContextFactory.config"\/>\n\t<\/Loggers>/g' /openhab/userdata/etc/log4j2.xml
+fi
