df2c21cc08f474e5c5d26c8a6559b3a875daa192
HolgerHees <holger.hees@gmail.com>
2024-09-14 13:27:24 +0200

    unified openhab custom value handling

--- roles/openhab/templates/python/configuration.py
+++ roles/openhab/templates/python/configuration.py
@@ -6,3 +6,5 @@ userConfigs = {
   {% if loop.index > 1 %},{% endif %}"{{username}}": { "state_item": {% if userdata[username].openhab.state_item is defined %}"{{userdata[username].openhab.state_item}}"{% else %}None{% endif %}, "notification_config": {% if userdata[username].openhab.notification_config is defined %}[ '{{userdata[username].openhab.notification_config | join("','") }}' ]{% else %}None{% endif %}, "is_admin": {{ 'True' if 'admin' in userdata[username].groups else 'False'}}, "name": "{{userdata[username].name}}" }
 {% endif %}{% endfor %}
 }
+
+customConfigs = {{ openhab_custom_value_map | to_nice_json }}
