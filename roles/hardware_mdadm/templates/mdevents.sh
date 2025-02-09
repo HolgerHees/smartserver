6173bbfeee4a0cbe3e45e0914ede7efe583b7761
HolgerHees <holger.hees@gmail.com>
2025-02-09 16:11:39 +0100

    fix mdadm event script

--- roles/hardware_mdadm/templates/mdevents.sh
+++ roles/hardware_mdadm/templates/mdevents.sh
@@ -6,13 +6,13 @@ device=$2
 state=$(cat /proc/mdstat)
 
 if [ "$event" = "Fail" ]; then
-    printf "Subject: RAID: A failure has been detected on $device\n\n$state" | sendmail root
+    echo -e "Subject: RAID: A failure has been detected on $device\n\n$state" | sendmail root
 elif [ "$event" = "FailSpare" ]; then
-    printf "Subject: RAID: A failure has been detected on spare $device\n\n$state" | sendmail root
+    echo -e "Subject: RAID: A failure has been detected on spare $device\n\n$state" | sendmail root
 elif [ "$event" = "DegradedArray" ]; then
-    printf "Subject: RAID: A degraded array has been detected on $device\n\n$state" | sendmail root
+    echo -e "Subject: RAID: A degraded array has been detected on $device\n\n$state" | sendmail root
 elif [ "$event" = "TestMessage" ]; then
-    printf "A test message for $device" | systemd-cat -t mdadm -p 6
+    echo -e "A test message for $device" | systemd-cat -t mdadm -p 6
 #elif [ "$event" = "DeviceDisappeared" ]; then
 #elif [ "$event" = "RebuildStarted" ]; then
 #elif [ "$event" = "Rebuild20" ]; then
