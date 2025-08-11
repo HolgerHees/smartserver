defc15b4bfe8bb5c9d4b1334f4403a7e082370c3
HolgerHees <holger.hees@gmail.com>
2025-08-11 14:05:12 +0200

    cleanup openwrt

--- roles/openwrt/templates/webui/openwrt.js
+++
@@ -1,4 +0,0 @@
-var subGroup = mx.Menu.getMainGroup('admin').getSubGroup('devices');
-{% for device in openwrt_devices %}
-subGroup.addUrl('openwrt_ap_{{device["host"]}}', ['admin'], 'https://{{device["host"]}}', { 'order': 460, 'title': '{{device["name"] if device["name"] is defined else device["host"]}}', 'info': '{i18n_OpenWRT Device}', 'icon': 'device_{{"wifi" if "ap" in device.config.openwrt.deployment_roles else "switch"}}.svg', 'target': '_blank' });
-{% endfor %}
