1312575d84524ba0c0f744eeb1d84eb2b4ef7dbd
HolgerHees <holger.hees@gmail.com>
2026-02-16 13:36:05 +0100

    BREAKING CHANGE in weather service
    ALTER TABLE `weather_forecast` CHANGE `maxWindSpeedInKilometerPerHour` `windGustInKilometerPerHour` DECIMAL(10,1) NOT NULL;
    ALTER TABLE `weather_forecast` CHANGE `effectiveCloudCoverInOcta` `cloudCoverInOcta` DECIMAL(10,1) NOT NULL;
    ALTER TABLE `weather_forecast` CHANGE `uvIndexWithClouds` `uvIndex` TINYINT(4) NOT NULL;
    ALTER TABLE `weather_forecast` CHANGE `precipitationProbabilityInPercent` `rainProbabilityInPercent` TINYINT(4) NOT NULL;
    ALTER TABLE `weather_forecast` CHANGE `precipitationAmountInMillimeter` `rainAmountInMillimeter` DOUBLE(10,1) NOT NULL;

--- roles/weather_service/templates/weather_forecast.sql
+++ roles/weather_service/templates/weather_forecast.sql
@@ -8,16 +8,16 @@ CREATE TABLE `weather_forecast` (
   `relativeHumidityInPercent` smallint(6) NOT NULL,
   `windSpeedInKilometerPerHour` decimal(10,1) NOT NULL,
   `windDirectionInDegree` smallint(6) NOT NULL,
-  `maxWindSpeedInKilometerPerHour` decimal(10,1) NOT NULL,
-  `effectiveCloudCoverInOcta` decimal(10,1) NOT NULL,
+  `windGustInKilometerPerHour` decimal(10,1) NOT NULL,
+  `cloudCoverInOcta` decimal(10,1) NOT NULL,
   `thunderstormProbabilityInPercent` tinyint(4) NOT NULL,
   `freezingRainProbabilityInPercent` tinyint(4) NOT NULL,
   `hailProbabilityInPercent` tinyint(4) NOT NULL,
   `snowfallProbabilityInPercent` tinyint(4) NOT NULL,
-  `precipitationProbabilityInPercent` tinyint(4) NOT NULL,
-  `precipitationAmountInMillimeter` double(10,1) NOT NULL,
+  `rainProbabilityInPercent` tinyint(4) NOT NULL,
+  `rainAmountInMillimeter` double(10,1) NOT NULL,
   `weatherCode` tinyint(4) NOT NULL,
-  `uvIndexWithClouds` tinyint(4) NOT NULL,
+  `uvIndex` tinyint(4) NOT NULL,
   `sunshineDurationInMinutes` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
